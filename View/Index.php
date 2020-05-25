ef67a23b7a76dfd156ee06414e5666b6621a9c49
Alfonsinius <43684079+Alfonsinius@users.noreply.github.com>
2020-05-24 23:29:50 -0600

    cambios

---
+++ View/Index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+  
+  <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+ <style>
+ .footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: black;
+   color: white;
+   text-align: center;
+}
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
+}
+
+.active {
+  background-color: #4CAF50;
+}
+</style>
+</head>
+<body style="background-color:#273746;">
+
+    <?php
+include '../View/Menu.html';
+?>
+
+
+<center>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/psZc1x1TQNI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</center>
+<br>
+<br>
+<br>
+<div class="footer">
+  <p>Tu destino soñado</p>
+   <p>Creadores: Josseline Matamoros</p>
+   <p>Alfonso Jiménez</p>
+	 <p>Versión 1.0</p>
+</div>
+
+</html>
